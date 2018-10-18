caadb591105efcf8bbac88dd400e7b1a8e7ce328
Diem Quynh LUong <luongdiemquynh1998@gmail.com>
2018-10-18 17:21:19 +0300

    add enemy

--- maze.py
+++ maze.py
@@ -6,12 +6,14 @@ win = turtle.Screen()
 win.bgcolor("white")
 win.title("Maze game")
 win.setup(width = 700,height = 700)
+win.tracer(0)
 
 #Register shape
 turtle.register_shape("ghost_left.gif")
 turtle.register_shape("ghost_right.gif")
 turtle.register_shape("treasure.gif")
 turtle.register_shape("wall.gif")
+turtle.register_shape("enemy.gif")
 
 #Create Pen
 class Pen(turtle.Turtle):
@@ -84,38 +86,86 @@ class Treasure(turtle.Turtle):
         self.goto(1000,1000)
         self.hideturtle()
 
+#Create Enemy
+class Enemy(turtle.Turtle):
+    def __init__(self,x,y):
+        turtle.Turtle.__init__(self)
+        self.shape("enemy.gif")
+        self.penup()
+        self.speed(0)
+        self.goto(x,y)
+        self.direction = random.choice(["up", "down", "left", "right"])
+
+    def move(self):
+        #Create change in enemy coordinate
+        if self.direction == "up":
+            dx = 0
+            dy = 24
+        elif self.direction == "down":
+            dx = 0
+            dy = -24
+        elif self.direction == "left":
+            dx = -24
+            dy = 0
+        elif self.direction == "right":
+            dx = 24
+            dy = 0
+        else:
+            dx = 0
+            dy = 0
+
+        #Calculate the coordinates after enemy movement
+        enemy_x = self.xcor() + dx
+        enemy_y = self.ycor() + dy
+        #Enemy Movement
+        if(enemy_x,enemy_y) not in walls:
+            self.goto(enemy_x, enemy_y)
+        else:
+            #Choose direction again
+            self.direction = random.choice(["up", "down", "left", "right"])
+
+        #Set timer for enemy to move next time
+        turtle.ontimer(self.move, t=random.randint(100,400))
+
+    def hideEnemy(self):
+        self.goto(1000,1000)
+        self.hideturtle()
+
 #Create Treasures
 treasures =[]
 
 #Create Levels
 levels = [""]
 
+#Create Enemies
+enemies = []
+
 #First level
 level1 = [
 "XXXXXXXXXXXXXXXXXXXXXXXXX",
-"X  XXXXXXX          P XXX",
+"X  XXXXXXE          XXXXX",
 "X  XXXXXXX  XXXXXX  XXXXX",
 "X       XX  XXXXXX  XXXXX",
-"X       XX  XXX        XX",
+"XP       XX  XXX        XX",
 "XXXXXX  XX  XXX        XX",
 "XXXXXX  XX  XXXXXX  XXXXX",
 "XXXXXX  XT    XXXX  XXXXX",
-"X  XXX        XXXX  XXXXX",
-"X  XXX  XXXXXXXXXXXXXXXXX",
+"X  XXX        XXXX  EXXXX",
+"X  XXX  XXXXXXXXXXTXXXXXX",
 "X         XXXXXXXXXXXXXXX",
-"X                XXXXXXXX",
+"X                EXXXXXXX",
 "XXXXXXXXXXXX     XXXXX  X",
 "XXXXXXXXXXXXXXX  XXXXX  X",
 "XXX  XXXXXXXXXX         X",
 "XXX                     X",
-"XXX         XXXXXXXXXXXXX",
+"XXX         EXXXXXXXXXXXX",
 "XXXXXXXXXX  XXXXXXXXXXXXX",
 "XXXXXXXXXX              X",
-"XX   XXXXX              X",
+"XXT   XXXXE             X",
 "XX   XXXXXXXXXXXXX  XXXXX",
 "XX    XXXXXXXXXXXX  XXXXX",
 "XX           XXXX       X",
-"XXXX                    X",
+"XXXE                    X",
 "XXXXXXXXXXXXXXXXXXXXXXXXX",
 ]
 
@@ -146,6 +196,10 @@ def SetupMaze(level):
             #Build treasure
             if block == "T":
                 treasures.append(Treasure(block_x, block_y))
+
+            #Build enemies
+            if block == "E":
+                enemies.append(Enemy(block_x, block_y))
                 
     
 #Create Pen instances
@@ -166,8 +220,13 @@ win.onkeypress(player.player_down,"Down")
 win.onkeypress(player.player_right,"Right")
 win.onkeypress(player.player_left,"Left")
 
+#Set timer to keep enemy moves instantly
+for enemy in enemies:
+    turtle.ontimer(enemy.move, t = 300)
+
 #Main Game loop
 while True:
+    #If player gets treasure
     for treasure in treasures:
         if player.collision(treasure):
             player.score = player.score + treasure.gold
@@ -176,6 +235,12 @@ while True:
             treasure.hideTreasure()
             #Remove treasure from treasures list
             treasures.remove(treasure)
+
+    #If player meets enemy
+    for enemy in enemies:
+        if player.collision(enemy):
+            print("You die")
+        
     win.update()
 
 
